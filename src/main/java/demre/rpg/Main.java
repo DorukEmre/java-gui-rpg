eebfd7d396df7ccd4ec49b1fe969a4be278d5084
Doruk Emre <doruk.emre@outlook.com>
2025-07-18 15:44:51 +0200

    Main initiliases MVC

--- src/main/java/demre/rpg/Main.java
+++ src/main/java/demre/rpg/Main.java
@@ -1,13 +1,41 @@
 package demre.rpg;
 
-import demre.rpg.controller.GameController;
 import demre.rpg.model.GameEngine;
+import demre.rpg.controller.GameController;
+import demre.rpg.view.GameView;
+import demre.rpg.view.ConsoleView;
+import demre.rpg.view.GUIView;
 
 public class Main {
   public static String gameMode = "console";
 
   public static void main(String[] args) {
-    // Check command line arguments for game mode
+
+    checkArgsAndGetGameMode(args);
+
+    GameEngine gameEngine = new GameEngine();
+
+    GameController gameController = new GameController(gameEngine);
+    gameController.initialiseGame();
+
+    GameView gameView;
+    if (gameMode.equals("gui")) {
+      System.out.println("Starting GUI mode...");
+      // Initialise GUI components here
+      gameView = new GUIView(gameEngine);
+    } else {
+      System.out.println("Starting Console mode...");
+      // Initialise console components here
+      gameView = new ConsoleView(gameEngine);
+    }
+
+    gameEngine.startGame(gameView);
+
+    System.exit(0);
+  }
+
+  // Check command line arguments for game mode
+  private static void checkArgsAndGetGameMode(String[] args) {
     if (args.length == 1 && args[0].equals("gui")) {
       gameMode = "gui";
     } else if (args.length == 1 && args[0].equals("console")) {
@@ -16,13 +44,6 @@ public class Main {
       System.out.println("Usage: java -jar swingy.jar [gui|console]");
       System.exit(1);
     }
-
     System.out.println("Hello, swingy in mode: " + gameMode);
-
-    GameEngine gameEngine = new GameEngine();
-    GameController gameController = new GameController(gameEngine);
-    gameController.initialiseGame();
-
-    System.exit(0);
   }
 }
