64a86ee2ee4455228f71b7848e6715a52776b72c
Doruk Emre <doruk.emre@outlook.com>
2025-07-20 14:44:50 +0200

    Added constructor for Armor, Helm, and Weapon classes

--- src/main/java/demre/rpg/model/items/Armor.java
+++ src/main/java/demre/rpg/model/items/Armor.java
@@ -3,6 +3,10 @@ package demre.rpg.model.items;
 public class Armor extends Item {
   // increases defense
 
+  public Armor(String name) {
+    super("Armor", name, 2);
+  }
+
   public Armor(String name, int modifier) {
     super("Armor", name, modifier);
   }
--- src/main/java/demre/rpg/model/items/Weapon.java
+++ src/main/java/demre/rpg/model/items/Weapon.java
@@ -3,6 +3,10 @@ package demre.rpg.model.items;
 public class Weapon extends Item {
   // increases the attack
 
+  public Weapon(String name) {
+    super("Weapon", name, 2);
+  }
+
   public Weapon(String name, int modifier) {
     super("Weapon", name, modifier);
   }
