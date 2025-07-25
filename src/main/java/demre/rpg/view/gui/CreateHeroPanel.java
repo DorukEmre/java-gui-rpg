feaa0180bdb2e301e2d21198d2137628b01d8dd0
Doruk Emre <doruk.emre@outlook.com>
2025-07-25 15:46:48 +0200

    Created files for the other GUI panels

---
+++ src/main/java/demre/rpg/view/gui/CreateHeroPanel.java
@@ -0,0 +1,13 @@
+package demre.rpg.view.gui;
+
+import java.awt.Font;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class CreateHeroPanel extends JPanel {
+
+}
